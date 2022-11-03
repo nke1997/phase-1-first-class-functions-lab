5ed4f15715f912dd2f0a782593ed4b549616a2e4
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-03 09:11:45 -0400

    commit tree 766b1795bdf0125ed22a206c8510948e1728ec7d

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driverArray) {
+    return driverArray.slice(0, 2);
+};
+
+returnFirstTwoDrivers(["Anna", "Carey", "Sammy", "Terence"]);
